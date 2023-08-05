aded5a3478a5a66b41acb4113310a39fa2ac6a4f
yash756 <penteyash@gmail.com>
2023-08-05 23:57:34 +0530

    step 6 to 8 done

--- src/App.js
+++ src/App.js
@@ -1,65 +1,67 @@
 import React from 'react';
 
-const Header = (props) => {
-    return <h1>{props.course}</h1>;
+const Header = ({ course }) => {
+    return <h1>{course}</h1>;
 };
 
-const Content = (props) => {
+const Content = ({ parts }) => {
     return (
         <div>
-            <Part
-                partName={props.parts[0].name}
-                partExercise={props.parts[0].exercises}
-            />
-            <Part
-                partName={props.parts[1].name}
-                partExercise={props.parts[1].exercises}
-            />
-            <Part
-                partName={props.parts[2].name}
-                partExercise={props.parts[2].exercises}
-            />
+            {parts.map((part) => (
+                <Part
+                    key={part.id}
+                    partName={part.name}
+                    partExercise={part.exercises}
+                />
+            ))}
         </div>
     );
 };
 
-const Part = (props) => {
+const Part = ({ partName, partExercise }) => {
     return (
         <p>
-            {props.partName} {props.partExercise}
+            {partName} {partExercise}
         </p>
     );
 };
 
-const Total = (props) => {
+const Total = ({ parts }) => {
     return (
         <p>
             Number of exercises{' '}
-            {props.parts[0].exercises +
-                props.parts[1].exercises +
-                props.parts[2].exercises}
+            {parts.reduce((acc, curr) => acc + curr.exercises, 0)}
         </p>
     );
 };
 
 const App = () => {
     const course = {
+        id: 1,
         name: 'half stack application development',
 
         parts: [
             {
                 name: 'fundamentals of react',
                 exercises: 10,
+                id: 1,
             },
 
             {
                 name: 'using props to pass data',
                 exercises: 7,
+                id: 2,
             },
 
             {
                 name: 'state of component',
                 exercises: 14,
+                id: 3,
+            },
+            {
+                name: 'redux',
+                exercises: 11,
+                id: 4,
             },
         ],
     };
